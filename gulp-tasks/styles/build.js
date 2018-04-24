6b05fa151ddd8b7a9d35cac151fce68167eff973
jbeltrami <joao@primarydesign.com>
2018-04-24 12:26:30 -0400

    Add gulp-autoprefixer to css build task.

--- gulp-tasks/styles/build.js
+++ gulp-tasks/styles/build.js
@@ -1,10 +1,12 @@
 const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
 const sass = require('gulp-sass');
 
 module.exports = gulp =>
   gulp.src(global.SOURCES_BASE_PATH + '/assets/styles/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({ style: 'compressed' }).on('error', sass.logError))
+    .pipe(autoprefixer())
     .pipe(gulp.dest(global.BASE_PATH + '/dist/assets/styles'))
     .pipe(global.BROWSER_SYNC.stream({ match: '**/*.css' }))
 ;
